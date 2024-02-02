d23160ffa211323948f35246793532469517059e
yehiahassanain <89047698+yehiahassanain@users.noreply.github.com>
2024-02-02 02:23:04 +0200

    Understanding Async Requests

--- Full-Application/controllers/admin.js
+++ Full-Application/controllers/admin.js
@@ -195,8 +195,8 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
   .then(product=>{
     if (!product){
@@ -207,13 +207,11 @@ exports.postDeleteProduct = (req, res, next) => {
   })
   .then(() => {
     console.log('DESTROYED PRODUCT');
-    res.redirect('/admin/products');
+    res.status(200).json({message: 'Success!'});
   })
   .catch(err => {
     // res.redirect('/500'); == 
-    const error = new Error(err);
-    error.httpStatusCode = 500;
-    return next(error);
+    res.status(500).json({message: 'Deleting product failed.'});
   })
   .catch(err=>next(err));
 };
